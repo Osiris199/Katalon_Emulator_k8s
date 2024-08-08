c28d47ec6ec8bffaae9d08ce6f1b5ee4db3e605c
Osiris199 <89781481+Osiris199@users.noreply.github.com>
2024-08-08 17:29:22 +0530

    Update delete_pod.sh

--- delete_pod.sh
+++ delete_pod.sh
@@ -62,6 +62,7 @@ control_c() {
 
 copy_reports() { 
     POD_NAME=$(minikube kubectl -- get pods --no-headers -o custom-columns=':metadata.name' | grep -i deployment)
+    echo "$POD_NAME"
     kubectl cp $POD_NAME:/empresa/Reports/. -c android-emulator /home/siddhatech/Reports
     echo "files transferred to /home/siddhatech/Reports path on local machine."
 }
